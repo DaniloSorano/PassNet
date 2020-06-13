13f63d09257b9b6db6d8f277d8341c34cbb731e7
Paolo Cintia <cintia@di.unipi.it>
2020-06-13 15:44:45 +0200

    Update features_extraction.sh

--- features_extraction.sh
+++ features_extraction.sh
@@ -1,5 +1,6 @@
 ## here we define the commands to extract features from input video
 
-## e.g. main.py "../Data/Video/" chievo_juve_1.mp4 25 tensor
+## e.g. main.py $1 chievo_juve_1.mp4 25 tensor
 
+##$1 = path of video input
 
